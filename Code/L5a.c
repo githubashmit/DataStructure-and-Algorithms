707a14e93fc8c6437bf3b5e10e36f0087b636b7e
Ashmit Prajapati <ashmitgithub@gmail.com>
2023-06-18 09:57:07 +0530

    18-06-2023

---
+++ Code/L5a.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void func(int n)
+{
+    int sum =0;
+    int product =1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d, %d\n", i ,j);
+        }
+        
+    }
+    
+}
+int main()
+{
+    func(6);
+    return 0;
+}
